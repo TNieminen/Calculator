1ab19d7c9f1d4404237dabe7bef38eb75a8361c7
Tuomo Nieminen <tuomo.a.nieminen@tuni.fi>
2020-10-23 14:40:46 +0800

    chore: cleaned up whitelist

--- backend/utils/whitelist.js
+++ backend/utils/whitelist.js
@@ -2,11 +2,11 @@
 // test the match strings at
 // https://regex101.com/
 const development = [
-  /http:\/\/192\.168\.[0-9]+\.[0-9]+/, // allow connections from local network to dev server, usefull for mobile debugging
+  /http:\/\/192\.168\.[0-9]+\.[0-9]+/, // allow connections from local network to dev server, useful for mobile debugging
   /http:\/\/localhost:[0-9]+$/, // localhost
 ]
 const production = [
-  /http:\/\/192\.168\.[0-9]+\.[0-9]+/, // allow connections from local network to dev server, usefull for mobile debugging
+  /http:\/\/192\.168\.[0-9]+\.[0-9]+/, // allow connections from local network to dev server, useful for mobile debugging
   /http:\/\/localhost:[0-9]+$/, // localhost
 ]
 
