d96a7ad087ce939ff6212aaee20f88fc41390ebb
Tuomo Nieminen <tuomo.a.nieminen@tuni.fi>
2020-10-23 14:39:10 +0800

    chore: improved linting

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -15,6 +15,13 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "semi": ["error", "never"],
+    // "no-mixed-spaces-and-tabs": "error",
+    // spacing
+    "space-before-blocks": ["error", "always"],
+    "object-curly-spacing": ["error", "always"],
+    "space-before-function-paren": ["error", "never"],
+    "func-call-spacing":["error","never"],
+    "arrow-spacing":["error", { "before": true, "after": true }]
   },
   overrides: [
     {
@@ -22,6 +29,10 @@ module.exports = {
         '**/__tests__/*.{j,t}s?(x)',
         '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
+      env: {
+        mocha: true,
+        node: true
+      },
     },
   ],
 }
