c0405475581bd7d4b8f29f3836359f7bfb85d05d
Tuomo Nieminen <tuomo.a.nieminen@tuni.fi>
2020-10-26 18:22:51 +0800

    frontend-chore: debugging test runner

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,9 +8,7 @@ import Chakra, {
 } from "@chakra-ui/vue"
 import { faCalculator,  } from '@fortawesome/free-solid-svg-icons'
 import App from "./App.vue"
-// import "dotenv-defaults/config"
 
-// Import Chakra UI Plugin and register it.
 Vue.use(Chakra,{
   icons:{
     iconPack:'fa',
@@ -24,11 +22,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render(h) {
-    // Mount our application inside the
-    // ThemeProvider and ColorModeProvider components :)
-
-    // You can also do this inside the App.vue file if you wish :)
-    // See example at https://github.com/chakra-ui/chakra-ui-vue#usage
     return h(CThemeProvider, [
       h(CColorModeProvider, [h(CBox, [h(CReset), h(App)])])
     ])
